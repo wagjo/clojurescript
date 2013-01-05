46b4660c21214e713a1c75c128f2c9e91fcdd32b
Jozef Wagner <jozef.wagner@gmail.com>
2013-01-05 19:24:43 +0100

    project clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject com.wagjo/clojurescript "0.1.0-SNAPSHOT"
+  :description "Fork of a ClojureScript with small adjustments."
+  :url "https://github.com/wagjo/clojurescript"
+  :source-paths ["src/clj" "src/cljs"]
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as ClojureScript"})
