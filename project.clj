7a987b55acc7ad26f3ce6d26e9512bf77d2a8703
Jozef Wagner <jozef.wagner@gmail.com>
2013-02-18 14:32:45 +0100

    deps

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject com.wagjo/clojurescript "0.1.0-SNAPSHOT"
   :description "Fork of a ClojureScript with small adjustments."
   :url "https://github.com/wagjo/clojurescript"
+  :dependencies [[com.wagjo/closure-library "0.1.0-SNAPSHOT"]
+                 ;; http://code.google.com/p/closure-compiler/wiki/Maven
+                 [com.google.javascript/closure-compiler "r2388"]
+                 [org.mozilla/rhino "1.7R4"]]
   :source-paths ["src/clj" "src/cljs"]
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
