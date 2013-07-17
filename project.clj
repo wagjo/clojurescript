e758d257d516007ad64d47482619e9afe41157f1
Jozef Wagner <jozef.wagner@gmail.com>
2013-07-17 15:37:17 +0200

    exclude project.clj from jar

--- project.clj
+++ project.clj
@@ -9,4 +9,5 @@
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
-            :comments "same as ClojureScript"})
+            :comments "same as ClojureScript"}
+  :jar-exclusions [#"project.clj"])
